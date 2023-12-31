e497dca09c72b4d253b21df010519e35807f988e
Martosky <imoniaromartins@yahoo.com>
2023-12-31 23:22:23 +0100

    finishing up the ui of my todo app

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-const input = document.querySelector("#input-text")
+const input = document.querySelector("#input-text");
+const button = document.getElementById("add-btn");
+const output = document.getElementById("output")
