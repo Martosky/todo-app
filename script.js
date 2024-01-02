5b3a0ad530af950e7629d8e94a51922402479978
Martosky <imoniaromartins@yahoo.com>
2024-01-02 17:26:43 +0100

    added some function to enable me input some tasks

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const input = document.querySelector("#input-text");
+const input = document.getElementById("input-text");
 const button = document.getElementById("add-btn");
-const output = document.getElementById("output")
+const output = document.getElementById("output");
+
+
+
+const myFunction = () => {
+
+    if (input.value === ""){
+        alert("Please fill in the input field with a task!");
+    }else {
+        let li = document.createElement("li");
+        li.innerText = input.value;
+        output.appendChild(li);
+        
+    }
+    input.value = "";
+}
